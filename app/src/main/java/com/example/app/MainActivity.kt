f1d940c848ade11a579cb470076614c794742d60
Linus <zerolecture0922@gmail.com>
2022-03-22 22:27:14 +0900

    Design : Appbar 구현 완료

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,39 +1,36 @@
 package com.example.app
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings.*
-import android.view.View
-import android.widget.Button
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
+
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val permissionBtn = findViewById<Button>(R.id.permission_btn)
-        permissionBtn.setOnClickListener {
-            val intent = Intent(this, PermissionActivity::class.java)
-            startActivity(intent)
-        }
+        setAppbar()
 
-        // 미션을 위해 잠시 주석 처리
         val colorData = Color.make(mutableListOf()).fillList()
         val myRV = findViewById<RecyclerView>(R.id.recycler_view)
         val myAdapter = MyAdapter(colorData)
         myRV.adapter = myAdapter
 
+    }
+
+    private fun setAppbar() {
+        val appBar = findViewById<MaterialToolbar>(R.id.appbar)
+        appBar.setOnMenuItemClickListener {
+            if (it.itemId == R.id.ic_permission) {
+                val intent = Intent(this, PermissionActivity::class.java)
+                startActivity(intent)
+                return@setOnMenuItemClickListener true
+            }
+            false
+        }
     }
 
 
