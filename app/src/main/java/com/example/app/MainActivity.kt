d63dc329090883ad3e92d2b55b8af9aef8aff226
Linus <zerolecture0922@gmail.com>
2022-03-22 21:45:19 +0900

    Feat : 권한 요청을 2번 거절하면 설정창으로 가도록 유도 완료

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -3,38 +3,41 @@ package com.example.app
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.*
+import android.view.View
 import android.widget.Button
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imageLoadingButton: Button
+    private val permissionLauncher = requestPermissionLauncher()
+    private val permissionLauncherForSecond = requestPermissionLauncherSecond()
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val test = requestPermissionLauncher()
+//        val test = requestPermissionLauncher()
         // 미션을 위해 잠시 주석 처리
         /*val colorData = Color.make(mutableListOf()).fillList()
         val myRV = findViewById<RecyclerView>(R.id.recycler_view)
         val myAdapter = MyAdapter(colorData)
         myRV.adapter = myAdapter*/
+
         imageLoadingButton = findViewById(R.id.album_btn)
         imageLoadingButton.setOnClickListener {
-            requestPermissionLauncher()
-            loadImageFromContent(test)
+            loadImageFromContent()
         }
+
     }
 
     private fun requestPermissionLauncher() =
@@ -51,73 +54,65 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    private fun requestPermissionLauncherSecond() =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                val loadImage = Intent(Intent.ACTION_GET_CONTENT).apply {
+                    this.type = "image/*"
+                }
+                startActivity(loadImage)
+            } else {
+                imageLoadingButton.showSnackbar(
+                    "이미지를 불러오기 위해 권한을 승인하시기 바랍니다",
+                    Snackbar.LENGTH_SHORT,
+                    "설정창으로 가기"
+                ) {
+                    val settingIntent = Intent().apply {
+                        this.action = ACTION_APPLICATION_DETAILS_SETTINGS
+                        this.data = Uri.fromParts("package", packageName, null)
+                    }
+                    startActivity(settingIntent)
+                }
+            }
+        }
+
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun loadImageFromContent(launcher: ActivityResultLauncher<String>) {
+    private fun loadImageFromContent() {
         if (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_MEDIA_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             Snackbar.make(imageLoadingButton, "permission Granted", Snackbar.LENGTH_LONG).show()
-            launcher.launch("android.permission.ACCESS_MEDIA_LOCATION")
+            permissionLauncher.launch("android.permission.ACCESS_MEDIA_LOCATION")
         } else {
-            requestMediaPermission(launcher)
+            requestMediaPermission()
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun requestMediaPermission(launcher: ActivityResultLauncher<String>) {
+    private fun requestMediaPermission() {
         if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_MEDIA_LOCATION)) {
-            Snackbar.make(
-                imageLoadingButton, "permission 2nd denied", Snackbar.LENGTH_LONG
-            ).show()
-
-            launcher.launch("android.permission.ACCESS_MEDIA_LOCATION")
-
-//            val settingIntent = Intent().apply {
-//                this.action = ACTION_APPLICATION_DETAILS_SETTINGS
-//            }
-//            startActivity(settingIntent)
+            permissionLauncherForSecond.launch("android.permission.ACCESS_MEDIA_LOCATION")
         } else {
-            launcher.launch("android.permission.ACCESS_MEDIA_LOCATION")
+            permissionLauncher.launch("android.permission.ACCESS_MEDIA_LOCATION")
         }
     }
 
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        when(requestCode) {
-//            0 -> {
-//                if(grantResults.isNotEmpty()) {
-//                    var isAllGranted = true
-//                    grantResults.forEach { isGranted ->
-//                        if(isGranted != PackageManager.PERMISSION_GRANTED) {
-//                            isAllGranted = false
-//                            return@forEach
-//                        }
-//                    }
-//                    if(isAllGranted) {
-//                        val loadImage = Intent(Intent.ACTION_GET_CONTENT).apply {
-//                            this.type = "image/*"
-//                        }
-//                        startActivity(loadImage)
-//                    } else {
-//                        if(!ActivityCompat.shouldShowRequestPermissionRationale(this,
-//                            Manifest.permission.ACCESS_MEDIA_LOCATION)) {
-//                            ActivityCompat.requestPermissions(this, permissions, 0)
-//                        }
-//                        else {
-//                            val settingIntent = Intent().apply {
-//                                this.action = ACTION_APPLICATION_DETAILS_SETTINGS
-//                            }
-//                            startActivity(settingIntent)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
+}
+
+fun View.showSnackbar(
+    msg: String,
+    length: Int,
+    actionMessage: CharSequence?,
+    action: (View) -> Unit
+) {
+    val snackbar = Snackbar.make(this, msg, length)
+    if (actionMessage != null) {
+        snackbar.setAction(actionMessage) {
+            action(this)
+        }.show()
+    }
 }
